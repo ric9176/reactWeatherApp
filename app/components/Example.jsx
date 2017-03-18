b82c57d97ea6514a2e37b32b953dd00f55957176
Richard Moss <richard@englishtutorslive.com>
2017-03-18 11:54:45 +0000

    Adds responsive grid class to main component and updates examples with links to homepage

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 const Example = () => {
   return (
-    <h3>Example Component</h3>
+    <div>
+      <h1 className="text-center">Example Component</h1>
+      <p>Here are a few examples locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=London'>London</Link>
+        </li>
+        <li>
+          <Link to='/?location=Cascais'>Cascais, Portugal</Link>
+
+        </li>
+      </ol>
+    </div>
   );
 };
 
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -4,8 +4,12 @@ import Nav from 'Nav';
 const Main = (props) => {
   return (
     <div>
-      <Nav></Nav>
-      {props.children}
+      <Nav/>
+      <div className="row">
+        <div className="columns medium-6 large-4 small-centered">
+          {props.children}
+        </div>
+      </div>
     </div>
   );
 };
