d38a179600c2ccf7949a10d761065a92bd6a55a1
Richard Moss <richard@englishtutorslive.com>
2017-03-23 15:44:18 +0000

    Hooks up search bar and example links

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,7 +4,13 @@ import {Link, IndexLink} from 'react-router';
 const Nav = React.createClass({
   onSearch (e) {
     e.preventDefault();
-    alert('Not yet wired up!');
+    let location = this.refs.search.value;
+    let encodedLocation = encodeURIComponent(location);
+
+    if (location.length > 0) {
+      this.refs.search.value = '';
+      window.location.hash = '#/?location=' + encodedLocation;
+    }
   },
   render () {
     return (
@@ -27,7 +33,7 @@ const Nav = React.createClass({
           <form onSubmit={this.onSearch}>
           <ul className="menu">
             <li>
-              <input type="search" placeholder="Search weather by city" />
+              <input type="search" ref="search" placeholder="Search weather by city" />
             </li>
             <li>
               <input type="submit" className="button" value="Get Weather" />
--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -15,7 +15,9 @@ const Weather = React.createClass({
 
     this.setState({
       isLoading: true,
-      errorMessage: undefined
+      errorMessage: undefined,
+      location: undefined,
+      temp: undefined
     });
 
     openWeatherMap.getTemp(location).then(function (temp) {
@@ -31,6 +33,22 @@ const Weather = React.createClass({
       });
     });
 
+  },
+  componentDidMount () {
+    let location = this.props.location.query.location;
+
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash = '#/';
+    }
+  },
+  componentWillReceiveProps (newProps) {
+    let location = newProps.location.query.location;
+
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash = '#/';
+    }
   },
   render() {
     var {isLoading, temp, location, errorMessage} = this.state;
